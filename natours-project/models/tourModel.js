a3301275ad8c5cb84fe8330383393a88e0b218cc
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-01-16 19:26:39 +0100

    Some refactoring of the model and extracting utils file

--- natours-project/models/tourModel.js
+++ natours-project/models/tourModel.js
@@ -9,20 +9,37 @@ const toursSchema = new mongoose.Schema({
     maxlength: [50, 'A tour name must have less or equal than 50 characters'],
     minlength: [5, 'A tour name must have more or equal than 5 characters'],
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size '],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
     min: [0, 'Price must be above 0'],
   },
-  duration: {
+  priceDiscount: {
+    type: Number,
+  },
+  summery: {
+    type: String,
+    trim: true,
+  },
+  description: {
     type: String, // Consider using Number if you want to store it as a number (e.g., in days)
-    required: [true, 'A tour must have a duration'],
+    required: [true, 'A tour must have a description'],
   },
   difficulty: {
     type: String,
@@ -32,6 +49,16 @@ const toursSchema = new mongoose.Schema({
       message: 'Difficulty must be either easy, medium, or difficult',
     },
   },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
--- natours-project/utils/utils.js
+++ natours-project/utils/utils.js
@@ -19,14 +19,22 @@ exports.handleResponse = (res, data, message = 'success', status = 200) => {
 };
 
 exports.handleError = (res, error) => {
-  console.error('Error:', error);
   if (error.name === 'ValidationError') {
-    res.status(400).json({
+    return res.status(400).json({
       status: 'fail',
       message: error.message,
     });
+  } else if (error.code === 11000) {
+    // Check for duplicate key error
+    return res.status(400).json({
+      status: 'fail',
+      message: `Duplicate value for ${Object.keys(error.keyValue).join(
+        ', '
+      )}, please choose a different value.`,
+    });
   } else {
-    res.status(500).json({
+    console.log(error.message); // Log other types of errors
+    return res.status(500).json({
       status: 'error',
       message: 'Something went wrong on the server!',
     });
