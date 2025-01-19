0eb5633dabe888dbb139da9ebf2748b146573fb5
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-01-19 12:28:58 +0100

    Unhandled rejections

--- natours-project/server.js
+++ natours-project/server.js
@@ -26,6 +26,14 @@ mongoose
 // Start the server
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.message);
+  console.log('UNHANDLED REJECTION ! Shutting down ... ');
+  server.close(() => {
+    process.exit(1);
+  });
 });
