61f166b0449464c8505a6a8311a1b2b904821fd6
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-01-28 22:48:41 +0100

    calculating avg rating on tours

--- natours-project/models/reviewModel.js
+++ natours-project/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -41,6 +42,59 @@ reviewSchema.pre(/^find/, function (next) {
   //     select: 'name price description difficulty',
   //   });
   next();
+});
+
+// Static method to calculate average ratings for a specific tour
+reviewSchema.statics.calcAvgRatings = async function (tourId) {
+  // Aggregation pipeline to calculate statistics for the given tourId
+  const stats = await this.aggregate([
+    {
+      // Stage 1: Match reviews related to the specified tour
+      $match: { tour: tourId },
+    },
+    {
+      // Stage 2: Group the reviews by tour and calculate the count and average of ratings
+      $group: {
+        _id: '$tour', // Group by the 'tour' field
+        nRating: { $sum: 1 }, // Count the total number of reviews (nRating)
+        avgRating: { $avg: '$rating' }, // Calculate the average rating
+      },
+    },
+  ]);
+
+  // If no stats returned, just return
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // points to the current review
+  this.constructor.calcAvgRatings(this.tour);
+});
+
+// Calculating also when deleting and updating review ( solution around the problem)
+// findByIdAndUpdate
+// findByIdAndDelete
+// Pre-hook to capture the current review before it's updated or deleted
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  // Store the document that will be updated or deleted
+  this.r = await this.findOne();
+  next(); // Proceed with the update or delete operation
+});
+
+// Post-hook to recalculate ratings after an update or delete operation
+reviewSchema.post(/^findOneAnd/, async function () {
+  // Recalculate the average rating and review count for the associated tour
+  await this.r.constructor.calcAvgRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
