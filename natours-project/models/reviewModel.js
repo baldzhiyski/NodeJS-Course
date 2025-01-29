b527a85c366765952cbde7c1a8210fb59c4157ff
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-01-29 10:27:09 +0100

    preventing dupl reviews

--- natours-project/models/reviewModel.js
+++ natours-project/models/reviewModel.js
@@ -32,6 +32,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Each user can create one review on each tour
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
